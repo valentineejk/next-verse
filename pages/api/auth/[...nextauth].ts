527e4086f779376bb402a71028d81f77e7e00442
valentineejk <valentineejk@gmail.com>
2023-07-31 20:21:26 +0100

    added stripe customer creation

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import {PrismaAdapter} from "@next-auth/prisma-adapter";
+import {PrismaClient} from "@prisma/client";
+import Stripe from "stripe";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  events: {
+    createUser: async ({user}) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET as string, {
+        apiVersion: "2022-11-15",
+      });
+
+      //create stripe customer
+   if (user.email && user.name) {
+       const customer: Stripe.Customer = await stripe.customers.create({
+         email: user.email,
+         name: user.name,
+       });
+
+       //update user with stripe customerId
+       await prisma.user.update({
+         where: {id: user.id},
+         data: {stripeCustomerId: customer.id},
+       });
+
+   }
+    },
+  },
+});
